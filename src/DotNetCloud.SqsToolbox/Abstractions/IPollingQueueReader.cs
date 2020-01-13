fe8d2d40980222d87ebea1d0410f395248decda4
Steve Gordon <sgordon@hotmail.co.uk>
2020-01-13 07:22:32 +0000

    Defining API in an interface

---
+++ src/DotNetCloud.SqsToolbox/Abstractions/IPollingQueueReader.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Channels;
+
+namespace DotNetCloud.SqsToolbox.Abstractions
+{
+    /// <summary>
+    /// Once started, polls a queue for messages, writing them to a channel, until stopped.
+    /// </summary>
+    public interface IPollingQueueReader<T> where T : class
+    {
+        /// <summary>
+        /// The <see cref="ChannelReader"/> from which received messages can be read for processing.
+        /// </summary>
+        ChannelReader<T> ChannelReader { get; }
+
+        /// <summary>
+        /// Start polling the queue for messages.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Stop polling the queue for messages.
+        /// </summary>
+        void Stop();
+    }
+}
